5539d0f91da5c5a2f16f7954f2f7719c3b24306d
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-04-27 00:37:28 +0400

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "AgFPz8oACXd55ruswpL3TBIIGZRgfettB7JvrxY9EK9f3XEuSRY2IbQqnBnOqJ7Q9tiHDLhPqf7f3mv-AOnuX9u-YnSfSjmHnzNHV_4tri2XDmGFegIGHTz2URfL7RxwLvnvrQ8btIKXfy_Ed2E1vNLj9r_Pst85-EjAID0FCEj2iSGPVc1hhPZ5zxOcUI855OGmuBp4Iavx2Y93h1ZGnlQ-ig8j18mgSw6Rpxca-mxcIRi36XoQmZylaZI5Yp34Jf_aqoybci2kDK62EWeDy5hzQc8QodBJFyYU8Hq8ub_iupJkwUydyFhKvGsC17zLJ_8ROUuWgEtNBVTxWgfqRdlQJuNN4QAAAAFc06grAA")
 BOT_TOKEN = getenv("BOT_TOKEN", "6195626321:AAGE8qd6TtxzvELWZnqaZ1sVKaD0XdJFzgk")
-BOT_NAME = getenv("BOT_NAME", "𝙺𝚁𝙰𝚈𝚉𝙴𝙽 𝙰𝚂i𝚂𝚃𝙰𝙽") 
+BOT_NAME = getenv("BOT_NAME", "𝙺𝚁𝙰𝚈𝚉𝙴𝙽 𝙼𝚄𝚂i𝙲") 
 API_ID = int(getenv("API_ID", 22007754))
 API_HASH = getenv("API_HASH", "7f0d6b4d928155eda014acdb5a014620")
 BOT_USERNAME = getenv("BOT_USERNAME", "KrayzenMusicbot")
